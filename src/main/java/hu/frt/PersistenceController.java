5251264522f54e6e43f6dc90b57029f8d4a1d66d
fratertamas <fratertomi@gmail.com>
2020-09-19 09:09:19 +0200

    add GetMapper institution-read

--- src/main/java/hu/frt/PersistenceController.java
+++ src/main/java/hu/frt/PersistenceController.java
@@ -1,12 +1,15 @@
 package hu.frt;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.frt.EducationalInstitution;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import jdk.nashorn.internal.ir.WhileNode;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @RestController
 public class PersistenceController {
@@ -15,7 +18,7 @@ public class PersistenceController {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
 
-            File file = new File("C:\\Users\\Halacska-NB4\\dev\\educational-institution\\educatuinal_institution.json");
+            File file = new File("C:\\Users\\Halacska-NB4\\dev\\educational-institution\\educational_institution.json");
             PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
             objectMapper.writeValue(out, educationalInstitution);
             out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
@@ -25,5 +28,29 @@ public class PersistenceController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @GetMapping("institution-read")
+    public List<EducationalInstitution> read(@RequestParam String omIdentificationNumber){
+        List<EducationalInstitution> educationalInstitutions = new ArrayList<>();
+
+        try {
+            File file = new File("C:\\Users\\Halacska-NB4\\dev\\educational-institution\\educational_institution.json");
+
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()){
+                String nextLine = sc.nextLine();
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                EducationalInstitution value = mapper.readValue(nextLine, EducationalInstitution.class);
+                if (value.getInstitutionData().getOmIdentificationNumber().equals(omIdentificationNumber))
+                    educationalInstitutions.add(value);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return educationalInstitutions;
     }
 }
